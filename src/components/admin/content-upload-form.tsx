108bfba120956c09dac7b1addd651024dd07036f
jyh2610 <jogg7191@gmail.com>
2025-06-12 11:01:38 +0900

    좌표 수정

--- src/components/admin/content-upload-form.tsx
+++ src/components/admin/content-upload-form.tsx
@@ -59,7 +59,7 @@ interface Rectangle {
   y: number;
   width: number;
   height: number;
-  type: string;
+  type?: string;
 }
 const customStyles = {
   option: (provided: any, state: { isSelected: any }) => ({
@@ -304,18 +304,6 @@ export function ContentUploadForm() {
   // 이미지별 좌표 정보 업데이트 함수
   const updateImageCoordinates = (coordinates: Rectangle[][]) => {
     setImageCoordinates(coordinates);
-    console.log(coordinates);
-    // overlays 데이터 구조 변환
-    const formattedOverlays = coordinates.flatMap((imageCoords: Rectangle[], fileIndex: number) => 
-      imageCoords.map((coord: Rectangle) => ({
-        ...coord,
-        type: coord.type,
-        fixedText: '',
-        fileIndex,
-        alignment: 'center'
-      }))
-    );
-    
     // form state의 overlays 업데이트
     handleChange('overlays', coordinates);
   };
